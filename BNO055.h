350e5f898a69b3f7c801845b05341d1ccc0582dd
SambeiDePretto <38137395+SambeiDePretto@users.noreply.github.com>
2018-04-06 20:13:54 +0200

    Update BNO055.h

--- BNO055.h
+++ BNO055.h
@@ -13,9 +13,9 @@
 #include <util/delay.h>
 #include "I2C.h"
 
-#define ADDRESS_BNO055	
+//#define ADDRESS_BNO055	
 
-BNO055_PAGE_ID_ADDR                                     = 0X07,
+#define BNO055_PAGE_ID_ADDR                                     = 0X07,
 
 /* PAGE0 REGISTER DEFINITION START*/
 #define BNO055_CHIP_ID_ADDR                                     = 0x00,
@@ -77,108 +77,108 @@ BNO055_PAGE_ID_ADDR                                     = 0X07,
 #define BNO055_LINEAR_ACCEL_DATA_Z_MSB_ADDR                     = 0X2D,
 
 /* Gravity data registers */
-#define BNO055_GRAVITY_DATA_X_LSB_ADDR                          = 0X2E,
-#define BNO055_GRAVITY_DATA_X_MSB_ADDR                          = 0X2F,
-#define BNO055_GRAVITY_DATA_Y_LSB_ADDR                          = 0X30,
-#define BNO055_GRAVITY_DATA_Y_MSB_ADDR                          = 0X31,
-#define BNO055_GRAVITY_DATA_Z_LSB_ADDR                          = 0X32,
-#define BNO055_GRAVITY_DATA_Z_MSB_ADDR                          = 0X33,
+#define BNO055_GRAVITY_DATA_X_LSB_ADDR                          0x2E
+#define BNO055_GRAVITY_DATA_X_MSB_ADDR                          0x2F
+#define BNO055_GRAVITY_DATA_Y_LSB_ADDR                          0x30
+#define BNO055_GRAVITY_DATA_Y_MSB_ADDR                          0x31
+#define BNO055_GRAVITY_DATA_Z_LSB_ADDR                          0x32
+#define BNO055_GRAVITY_DATA_Z_MSB_ADDR                          0x33
 
 /* Temperature data register */
-#define BNO055_TEMP_ADDR                                        = 0X34,
+#define BNO055_TEMP_ADDR                                        0x34
 
 /* Status registers */
-#define BNO055_CALIB_STAT_ADDR                                  = 0X35,
-#define BNO055_SELFTEST_RESULT_ADDR                             = 0X36,
-#define BNO055_INTR_STAT_ADDR                                   = 0X37,
+#define BNO055_CALIB_STAT_ADDR                                  0x35
+#define BNO055_SELFTEST_RESULT_ADDR                             0x36
+#define BNO055_INTR_STAT_ADDR                                   0x37
 
-#define BNO055_SYS_CLK_STAT_ADDR                                = 0X38,
-#define BNO055_SYS_STAT_ADDR                                    = 0X39,
-#define BNO055_SYS_ERR_ADDR                                     = 0X3A,
+#define BNO055_SYS_CLK_STAT_ADDR                                0x38
+#define BNO055_SYS_STAT_ADDR                                    0x39
+#define BNO055_SYS_ERR_ADDR                                     0x3A
 
 /* Unit selection register */
-#define BNO055_UNIT_SEL_ADDR                                    = 0X3B,
-#define BNO055_DATA_SELECT_ADDR                                 = 0X3C,
+#define BNO055_UNIT_SEL_ADDR                                    0x3B
+#define BNO055_DATA_SELECT_ADDR                                 0x3C
 
 /* Mode registers */
-#define BNO055_OPR_MODE_ADDR                                    = 0X3D,
-#define BNO055_PWR_MODE_ADDR                                    = 0X3E,
+#define BNO055_OPR_MODE_ADDR                                    0x3D
+#define BNO055_PWR_MODE_ADDR                                    0x3E
 
-#define BNO055_SYS_TRIGGER_ADDR                                 = 0X3F,
-#define BNO055_TEMP_SOURCE_ADDR                                 = 0X40,
+#define BNO055_SYS_TRIGGER_ADDR                                 0x3F
+#define BNO055_TEMP_SOURCE_ADDR                                 0x40
 
 /* Axis remap registers */
-#define BNO055_AXIS_MAP_CONFIG_ADDR                             = 0X41,
-#define BNO055_AXIS_MAP_SIGN_ADDR                               = 0X42,
+#define BNO055_AXIS_MAP_CONFIG_ADDR                             0x41
+#define BNO055_AXIS_MAP_SIGN_ADDR                               0x42
 
 /* SIC registers */
-#define BNO055_SIC_MATRIX_0_LSB_ADDR                            = 0X43,
-#define BNO055_SIC_MATRIX_0_MSB_ADDR                            = 0X44,
-#define BNO055_SIC_MATRIX_1_LSB_ADDR                            = 0X45,
-#define BNO055_SIC_MATRIX_1_MSB_ADDR                            = 0X46,
-#define BNO055_SIC_MATRIX_2_LSB_ADDR                            = 0X47,
-#define BNO055_SIC_MATRIX_2_MSB_ADDR                            = 0X48,
-#define BNO055_SIC_MATRIX_3_LSB_ADDR                            = 0X49,
-#define BNO055_SIC_MATRIX_3_MSB_ADDR                            = 0X4A,
-#define BNO055_SIC_MATRIX_4_LSB_ADDR                            = 0X4B,
-#define BNO055_SIC_MATRIX_4_MSB_ADDR                            = 0X4C,
-#define BNO055_SIC_MATRIX_5_LSB_ADDR                            = 0X4D,
-#define BNO055_SIC_MATRIX_5_MSB_ADDR                            = 0X4E,
-#define BNO055_SIC_MATRIX_6_LSB_ADDR                            = 0X4F,
-#define BNO055_SIC_MATRIX_6_MSB_ADDR                            = 0X50,
-#define BNO055_SIC_MATRIX_7_LSB_ADDR                            = 0X51,
-#define BNO055_SIC_MATRIX_7_MSB_ADDR                            = 0X52,
-#define BNO055_SIC_MATRIX_8_LSB_ADDR                            = 0X53,
-#define BNO055_SIC_MATRIX_8_MSB_ADDR                            = 0X54,
+#define BNO055_SIC_MATRIX_0_LSB_ADDR                            0x43
+#define BNO055_SIC_MATRIX_0_MSB_ADDR                            0x44
+#define BNO055_SIC_MATRIX_1_LSB_ADDR                            0x45
+#define BNO055_SIC_MATRIX_1_MSB_ADDR                            0x46
+#define BNO055_SIC_MATRIX_2_LSB_ADDR                            0x47
+#define BNO055_SIC_MATRIX_2_MSB_ADDR                            0x48
+#define BNO055_SIC_MATRIX_3_LSB_ADDR                            0x49
+#define BNO055_SIC_MATRIX_3_MSB_ADDR                            0x4A
+#define BNO055_SIC_MATRIX_4_LSB_ADDR                            0x4B
+#define BNO055_SIC_MATRIX_4_MSB_ADDR                            0x4C
+#define BNO055_SIC_MATRIX_5_LSB_ADDR                            0x4D
+#define BNO055_SIC_MATRIX_5_MSB_ADDR                            0x4E
+#define BNO055_SIC_MATRIX_6_LSB_ADDR                            0x4F
+#define BNO055_SIC_MATRIX_6_MSB_ADDR                            0x50
+#define BNO055_SIC_MATRIX_7_LSB_ADDR                            0x51
+#define BNO055_SIC_MATRIX_7_MSB_ADDR                            0x52
+#define BNO055_SIC_MATRIX_8_LSB_ADDR                            0x53
+#define BNO055_SIC_MATRIX_8_MSB_ADDR                            0x54
 
 /* Accelerometer Offset registers */
-#define ACCEL_OFFSET_X_LSB_ADDR                                 = 0X55,
-#define ACCEL_OFFSET_X_MSB_ADDR                                 = 0X56,
-#define ACCEL_OFFSET_Y_LSB_ADDR                                 = 0X57,
-#define ACCEL_OFFSET_Y_MSB_ADDR                                 = 0X58,
-#define ACCEL_OFFSET_Z_LSB_ADDR                                 = 0X59,
-#define ACCEL_OFFSET_Z_MSB_ADDR                                 = 0X5A,
+#define ACCEL_OFFSET_X_LSB_ADDR                                 0x55
+#define ACCEL_OFFSET_X_MSB_ADDR                                 0x56
+#define ACCEL_OFFSET_Y_LSB_ADDR                                 0x57
+#define ACCEL_OFFSET_Y_MSB_ADDR                                 0x58
+#define ACCEL_OFFSET_Z_LSB_ADDR                                 0x59
+#define ACCEL_OFFSET_Z_MSB_ADDR                                 0x5A
 
 /* Magnetometer Offset registers */
-#define MAG_OFFSET_X_LSB_ADDR                                   = 0X5B,
-#define MAG_OFFSET_X_MSB_ADDR                                   = 0X5C,
-#define MAG_OFFSET_Y_LSB_ADDR                                   = 0X5D,
-#define MAG_OFFSET_Y_MSB_ADDR                                   = 0X5E,
-#define MAG_OFFSET_Z_LSB_ADDR                                   = 0X5F,
-#define MAG_OFFSET_Z_MSB_ADDR                                   = 0X60,
+#define MAG_OFFSET_X_LSB_ADDR                                   0x5B
+#define MAG_OFFSET_X_MSB_ADDR                                   0x5C
+#define MAG_OFFSET_Y_LSB_ADDR                                   0x5D
+#define MAG_OFFSET_Y_MSB_ADDR                                   0x5E
+#define MAG_OFFSET_Z_LSB_ADDR                                   0x5F
+#define MAG_OFFSET_Z_MSB_ADDR                                   0x60
 
 /* Gyroscope Offset register s*/
-#define GYRO_OFFSET_X_LSB_ADDR                                  = 0X61,
-#define GYRO_OFFSET_X_MSB_ADDR                                  = 0X62,
-#define GYRO_OFFSET_Y_LSB_ADDR                                  = 0X63,
-#define GYRO_OFFSET_Y_MSB_ADDR                                  = 0X64,
-#define GYRO_OFFSET_Z_LSB_ADDR                                  = 0X65,
-#define GYRO_OFFSET_Z_MSB_ADDR                                  = 0X66,
+#define GYRO_OFFSET_X_LSB_ADDR                                  0x61
+#define GYRO_OFFSET_X_MSB_ADDR                                  0x62
+#define GYRO_OFFSET_Y_LSB_ADDR                                  0x63
+#define GYRO_OFFSET_Y_MSB_ADDR                                  0x64
+#define GYRO_OFFSET_Z_LSB_ADDR                                  0x65
+#define GYRO_OFFSET_Z_MSB_ADDR                                  0x66
 
 /* Radius registers */
-#define ACCEL_RADIUS_LSB_ADDR                                   = 0X67,
-#define ACCEL_RADIUS_MSB_ADDR                                   = 0X68,
-#define MAG_RADIUS_LSB_ADDR                                     = 0X69,
-#define MAG_RADIUS_MSB_ADDR                                     = 0X6A
+#define ACCEL_RADIUS_LSB_ADDR                                   0x67
+#define ACCEL_RADIUS_MSB_ADDR                                   0x68
+#define MAG_RADIUS_LSB_ADDR                                     0x69
+#define MAG_RADIUS_MSB_ADDR                                     0x6A
 
-#define POWER_MODE_NORMAL                                       = 0X00,
-#define POWER_MODE_LOWPOWER                                     = 0X01,
-#define POWER_MODE_SUSPEND                                      = 0X02
+#define POWER_MODE_NORMAL                                       0x00
+#define POWER_MODE_LOWPOWER                                     0x01
+#define POWER_MODE_SUSPEND                                      0x02
 
 /* Operation mode settings*/
-#define OPERATION_MODE_CONFIG                                   = 0X00,
-#define OPERATION_MODE_ACCONLY                                  = 0X01,
-#define OPERATION_MODE_MAGONLY                                  = 0X02,
-#define OPERATION_MODE_GYRONLY                                  = 0X03,
-#define OPERATION_MODE_ACCMAG                                   = 0X04,
-#define  OPERATION_MODE_ACCGYRO                                 = 0X05,
-#define OPERATION_MODE_MAGGYRO                                  = 0X06,
-#define OPERATION_MODE_AMG                                      = 0X07,
-#define OPERATION_MODE_IMUPLUS                                  = 0X08,
-#define OPERATION_MODE_COMPASS                                  = 0X09,
-#define OPERATION_MODE_M4G                                      = 0X0A,
-#define OPERATION_MODE_NDOF_FMC_OFF                             = 0X0B,
-#define OPERATION_MODE_NDOF                                     = 0X0C
+#define OPERATION_MODE_CONFIG                                   0x00
+#define OPERATION_MODE_ACCONLY                                  0x01
+#define OPERATION_MODE_MAGONLY                                  0x02
+#define OPERATION_MODE_GYRONLY                                  0x03
+#define OPERATION_MODE_ACCMAG                                   0x04
+#define  OPERATION_MODE_ACCGYRO                                 0x05
+#define OPERATION_MODE_MAGGYRO                                  0x06
+#define OPERATION_MODE_AMG                                      0x07
+#define OPERATION_MODE_IMUPLUS                                  0x08
+#define OPERATION_MODE_COMPASS                                  0x09
+#define OPERATION_MODE_M4G                                      0x0A
+#define OPERATION_MODE_NDOF_FMC_OFF                             0x0B
+#define OPERATION_MODE_NDOF                                     0x0C
 
 
 
@@ -188,5 +188,6 @@ void writeReg_BNO();
 uint8_t readReg_BNO();
 
 int BNO_begin();
+void setMode();
 
 #endif /* BNO055_H_ */
